0b5265e9827dbd6fd228eec48db35823904573bf
Artem Ogorodnik <artemogorodnik2004@gmail.com>
2023-10-30 21:38:10 +0300

    Добавьте файлы проекта.

---
+++ OS 1/OS 1.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <process.h>
+using namespace std;
+
+
+class Object {
+
+public:
+	int n;
+	int* a;
+	int t;
+	int r;
+	Object(int* a_, int n_, int t_) : a(a_), n(n_), t(t_), r(0) {}
+
+};
+
+
+DWORD WINAPI Worker(LPVOID n)
+//unsigned __stdcall Worker(void* n)
+{
+	int count = 0;
+	for (int i = 0; i < ((Object*)n)->n; i++) {
+		if(((Object*)n)->a[i] % 2 == 1) {
+			count++;
+		}
+		Sleep(2 * 1000);
+	}
+	((Object*)n)->r = count;
+	return 0;
+}
+
+int main()
+{
+	HANDLE	hThread;
+	DWORD	IDThread;
+
+	int n = 0;
+	int t = 0;
+
+	cout << " Quantity numbers = ";
+
+	cin >> n;
+
+	int* a = new int[n];
+
+	cout << " Numbers = ";
+
+	for (int i = 0; i < (int)n; i++) {
+		cin >> a[i];
+	}
+
+	cout << " Time = ";
+
+	cin >> t;
+
+	auto A = new Object(a, n, t);
+
+	hThread = CreateThread(NULL, 0, Worker, (LPVOID)A, 0, &IDThread);
+	if (hThread == NULL)
+		return GetLastError();
+	
+	//hThread = (HANDLE)_beginthreadex(NULL, 0, &Worker, (void*)A , 0, (unsigned int*)&IDThread);
+
+	SuspendThread(hThread);
+	ResumeThread(hThread);
+	if (WaitForSingleObject(hThread, 1) != WAIT_OBJECT_0)
+	{
+		cout << " Wait for single object failed." << endl;
+	}
+	else
+	{
+		cout << " Result = " << A->r << '\n';
+	}
+
+	//WaitForSingleObject(hThread, INFINITE);
+
+	CloseHandle(hThread);
+}
